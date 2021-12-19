f14badca7624d230488cd9736a01b4bd0b2ad31a
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2021-12-19 17:37:03 -0500

    employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,10 @@
-const Employee = require('../lib/Employee')
+const Employee = require('../lib/Employee.js')
 
 
 test('creates an employee object', () => {
-    const employee = new Employee('Jim');
+    const employee = new Employee('bryan');
 
-    expect(employee.name).toBe('Jim');
+    expect(employee.name).toBe(expect.any(String));
     expect(employee.id).toBe(expect.any(String));
     expect(employee.email).toBe(expect.any(String));
 
