0d39f9b5bf0f805dff04a442f8a74ac610097d3b
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2021-12-19 18:05:19 -0500

    push

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,9 +4,9 @@ const Employee = require('../lib/Employee.js')
 test('creates an employee object', () => {
     const employee = new Employee('bryan');
 
-    expect(employee.name).toBe(expect.any(String));
-    expect(employee.id).toBe(expect.any(String));
-    expect(employee.email).toBe(expect.any(String));
+    expect(employee.name).toBe('bryan');
+    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.email).toEqual(expect.any(String));
 
 });
 
