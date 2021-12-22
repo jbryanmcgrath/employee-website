0f2bd11ac59c729261aa7c273de073e219537959
jbryanmcgrath <90098243+jbryanmcgrath@users.noreply.github.com>
2021-12-21 23:49:53 -0500

    more inquirer work done

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 
 
-const initiateApp = managerData => {
-    inquirer.prompt[{
+const initiateApp = () => {
+    return inquirer.prompt([{
         type: 'input',
         name: 'manager',
-        message: 'Start off by listing the manager. What is the managers name (Reguired)',
+        message: 'Start off by listing the manager. What is the managers name (Required)',
         validate: managerInput => {
             if (managerInput) {
                 return true;
@@ -46,11 +46,79 @@ const initiateApp = managerData => {
     {
         type: 'input',
         name: 'office',
-        message: 'Please enter the employee office number (Required)'
-    }]
+        message: 'Please enter the manager number (Required)'
+    }])
 }
+
+
+const employeeClassQuestions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'employee',
+        message: "What is the emplopyee's name (Reguired)",
+        validate: employeeInput => {
+            if (employeeInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of the employee.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the Employee ID? (Reguired)',
+        validate: idInput => {
+            if (idInput) {
+                return true
+            } else {
+                console.log('Please enter the Employee ID')
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the employee email? (Required) ',
+        validate: emailInput => {
+            if (emailInput) {
+                return true
+            } else {
+                console.log('Please enter a valid email address.')
+            }
+        }
+    }])
+};
+
+
+
+
+
+
+const addEmployee = () => {
+    return inquirer.prompt([{
+        type: 'list',
+        name: 'office',
+        message: 'Do you have any positions to add?',
+        choices: ['Employee', 'Engineer', 'Intern', 'No More Roles to Add'],
+        validate: officeList => {
+            if (officeList === 'No More Roles to Add') {
+                console.log('Thank you for using this app to generate your employee webpage. Have a great day');
+            }
+            if (officeList === 'Employee') {
+                employeeClassQuestions();
+            }
+        }
+    }])
+};
+
+
+
+
+
 initiateApp()
-    // .then(console.log(managerData))
+    .then(addEmployee)
 // const userInput = () => {
 //     inquirer.prompt[{
 //         type: 'list',
